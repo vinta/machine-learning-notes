ef3e3db726296d85a3c21a5e0ae6188b0d860e3c
Vinta <vinta.chen@gmail.com>
2016-10-30 21:08:12 +0800

    GaussianNB

--- Intro to Machine Learning/ud120-projects/naive_bayes/nb_author_id.py
+++ Intro to Machine Learning/ud120-projects/naive_bayes/nb_author_id.py
@@ -1,33 +1,48 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
-from time import time
 sys.path.append("../tools/")
+
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#########################################################
+# your code goes here ###
 
+from time import time
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+model = GaussianNB()
+
+t0 = time()
+model.fit(features_train, labels_train)
+t1 = time()
+print(t1 - t0)
+
+t0 = time()
+labels_prediction = model.predict(features_test)
+t1 = time()
+print(t1 - t0)
+
+accuracy = accuracy_score(labels_test, labels_prediction)
+print(accuracy)
 
 #########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
